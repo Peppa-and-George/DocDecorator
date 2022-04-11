98e7c817be38f43443310f738b302810dcd3631f
liwenkang <123qwe!@#QWE>
2022-04-11 15:37:06 +0800

    feature v1.0 添加文档修饰库

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# @Time: 21/3/2022 下午2:09
+# @Author: kang
+
+"""
+修饰文档的库
+
+结构：
+    part: 获取文档里面的各种元素
+    doc_decorate: 对文档进行修改
+"""
+
+VERSION = '1.0'
+__all__ = [
+    'doc_decorate',
+    'part',
+]
