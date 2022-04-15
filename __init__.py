fd40581de434b64725e1dfc8637eea72d5c507b3
liwenkang <123qwe!@#QWE>
2022-04-15 15:21:15 +0800

    feature v2.0.0 发布2.0.0版本

--- __init__.py
+++ __init__.py
@@ -5,12 +5,14 @@
 修饰文档的库
 
 结构：
-    part: 获取文档里面的各种元素
-    doc_decorate: 对文档进行修改
+    element: 获取文档元素
+    content: 文章内容增加,包含文档表格，文字，图片的增加，
+    style:  样式修改模块，和对文档特定元素的样式修改
 """
 
-VERSION = '1.0'
+VERSION = '2.0'
 __all__ = [
-    'doc_decorate',
-    'part',
+    'element',
+    'content',
+    'style',
 ]
